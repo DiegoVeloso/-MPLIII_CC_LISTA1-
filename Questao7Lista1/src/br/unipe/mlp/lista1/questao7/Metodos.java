4aeb89a559f7c903ebc597d696406598d67bbb3e
diegoveloso <pessoa_diego@hotmail.com>
2014-02-23 22:00:58 -0300

    Questao7

---
+++ Questao7Lista1/src/br/unipe/mlp/lista1/questao7/Metodos.java
@@ -0,0 +1,24 @@
+package br.unipe.mlp.lista1.questao7;
+
+import java.util.Scanner;
+
+public class Metodos{
+	
+	Scanner input= new Scanner(System.in);
+	
+
+	private Contato[]contato;
+
+	public Contato[] getContato() {
+		return contato;
+	}
+
+	public void setContato(Contato[] contato) {
+		this.contato = contato;
+	}
+	
+	public void adicionar(String nome, int numero){
+		
+	}
+
+}
---
+++ Questao7Lista1/src/br/unipe/mlp/lista1/questao7/Principal.java
@@ -0,0 +1,8 @@
+package br.unipe.mlp.lista1.questao7;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+	}
+
+}
