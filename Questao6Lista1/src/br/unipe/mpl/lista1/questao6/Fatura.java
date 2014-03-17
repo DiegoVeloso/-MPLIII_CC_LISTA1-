7b9a6e8ca0ee7c88a06bf1c64fecf99759bdaf80
diegoveloso <pessoa_diego@hotmail.com>
2014-03-17 20:44:26 -0300

    Questao 6

---
+++ Questao6Lista1/src/br/unipe/mpl/lista1/questao6/Fatura.java
@@ -0,0 +1,47 @@
+package br.unipe.mpl.lista1.questao6;
+
+public class Fatura {
+	private String numero;
+	private String descricao;
+	private int qntComprada;
+	private double preco;
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQntComprada() {
+		return qntComprada;
+	}
+	public void setQntComprada(int qntComprada) {
+		this.qntComprada = qntComprada;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public Fatura(String numero, String descricao, int qntComprada, double preco){
+		this.numero = numero;
+		this.descricao= descricao;
+		this.qntComprada = qntComprada;
+		this.preco = preco;
+	}
+	
+	//Método para calcular o valor da fatura
+	
+	public double getValorFatura(int qntComprada, double preco){
+		double valorTotal = (qntComprada * preco);
+		
+		return valorTotal;
+	}
+}
---
+++ Questao6Lista1/src/br/unipe/mpl/lista1/questao6/Principal.java
@@ -0,0 +1,17 @@
+package br.unipe.mpl.lista1.questao6;
+
+import java.util.Scanner;
+
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		Scanner input= new Scanner(System.in);
+		
+		System.out.println("Digite o nome do produto: ");
+		descricao= input.next();
+		
+		Fatura fatura= new Fatura();
+	
+}
+}
