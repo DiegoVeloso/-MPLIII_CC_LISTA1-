2bae5d8fed194ed2a59d071839573cc320907b14
diegoveloso <pessoa_diego@hotmail.com>
2014-03-20 00:33:46 -0300

    Questão 1

--- Questao1/src/br/unipe/mlp/lista1/questao1/QST1.java
+++ Questao1/src/br/unipe/mlp/lista1/questao1/QST1.java
@@ -1,5 +1,17 @@
 package br.unipe.mlp.lista1.questao1;
 
 public class QST1 {
+	
+	/* No primeiro passo do processo de compilação vai acontecer a compilação do programa em bytecodes. 
+	 * O compilador java converte o código em fonte em bytecodes que representam tarefas a serem executadas
+	 *  na fase de execução.
+	 *  
+	 *  A jvm armazena o programa na memória para executá-lo, efetuando o carregamento. Enquanto as classes são
+	 *  carregadas o verificador examina seus bytecodes para assegurar que são válidos e não violam restriçao de
+	 *  segurança do java.
+	 *  
+	 *  A jvm executam os bytecodes utilizando uma combinação de interpretação chamada compilação JIT(Just In time)
+	 *  conhecida como compilador java.
+	 *
 
 }
