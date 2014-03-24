88144ae3aacfc0d2a7b8f0c59a0bfcbda74a5e7a
diegoveloso <pessoa_diego@hotmail.com>
2014-03-23 23:14:05 -0300

    Questao 3

---
+++ Questao3Lista1/src/br/unipe/mpl/lista1/questao3/QST3.java
@@ -0,0 +1,19 @@
+package br.unipe.mpl.lista1.questao3;
+
+public class QST3 {
+	
+	/* Como definir e exemplificar programação orientada a objetos, como por exemplo:
+
+Os programas orientados a objetos funcionam numa organização a partir dos dados com o seguinte princípio-chave: " dados controlando
+o acesso ao código". Em uma linguagem orientada a objetos, você define os dados e as rotinas que podem atuar sobre eles. Logo, um tipo
+de dado define precisamente que tipo de operações pode ser aplicado a esses dados.
+	 * 
+	 * Benefícios da orientação a objetos: 
+	 
+	 * Acelerar otempo de desenvolvimento;
+       Reduzir o tempo de manutenção;
+       Mais fácil de entender e adaptar;
+       Código de melhor qualidade;
+	 */
+
+}
